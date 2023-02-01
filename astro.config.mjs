ae914ce3991f9f59a97bf3b97fc7de59bc8344ca
Joshua Byrd <phocks@gmail.com>
2023-02-01 16:06:56 +1000

    Workflows?

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://abcnews.github.io/",
+  site: "https://dev.phocks.org/",
   integrations: [
     tailwind({
       config: {
