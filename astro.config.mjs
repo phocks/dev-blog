c04e8c519e6ffeb7f41910e27dc6ab52f4df69d5
Joshua Byrd <phocks@gmail.com>
2023-02-01 16:52:48 +1000

    base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://phocks.github.io/dev-blog",
+  site: "https://phocks.github.io",
+  base: "/dev-blog",
   integrations: [
     tailwind({
       config: {
